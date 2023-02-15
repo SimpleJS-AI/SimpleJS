cb05ac23623b8beaef94593ca91ea5f494c3ee3b
flug8 <80972789+flug8@users.noreply.github.com>
2023-02-15 23:09:16 +0100

    First commit

---
+++ script.js
@@ -0,0 +1,14 @@
+
+
+// Split title into letters (used for animation)
+
+const titleElements = document.querySelectorAll('#title div .right');
+
+for (const e of titleElements) {
+    const text = e.innerHTML;
+    let textF = '';
+    for (const c of text) {
+        textF += `<span>${c}</span>`;
+    }
+    e.innerHTML = textF;
+}
