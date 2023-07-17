c4535250c8a30e9acca6b2995bfb03d19d31c041
flug8 <80972789+flug8@users.noreply.github.com>
2023-07-17 12:32:11 +0200

    added colors.json

--- script.js
+++ script.js
@@ -23,3 +23,20 @@ function enableScroll(pos) {
     document.body.style.overflow = 'auto';
     window.scrollTo(0, pos);
 }
+
+setColorPalette();
+
+function setColorPalette() {
+    fetch('https://flug8.github.io/SimpleJS/colors.json')
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+            /*data.forEach((color, i) => {
+                document.documentElement.style.setProperty(`--c${i}`, color);
+            });*/
+        });
+}
+
+setTimeout(() => {
+    window.parent.postMessage(window.getComputedStyle(document.documentElement).getPropertyValue('--c1'), '*');
+}, 2000);
