afa32e80444b81f93456a7c71477429f8e8ab94b
flug8 <80972789+flug8@users.noreply.github.com>
2023-08-01 13:04:42 +0200

    updated Welcome Screen, added structure for content

--- script.js
+++ script.js
@@ -2,13 +2,13 @@
 window.scrollTo(0, 0);
 // Check if the user agent is from a Chromium desktop browser
 function isChromiumDesktop() {
-    var userAgent = navigator.userAgent.toLowerCase();
+    let userAgent = navigator.userAgent.toLowerCase();
     return /chrome|edge|opera/.test(userAgent) && !/android/.test(userAgent);
 }
 
 // Check if the user agent is from Chromium for Android
 function isChromiumAndroid() {
-    var userAgent = navigator.userAgent.toLowerCase();
+    let userAgent = navigator.userAgent.toLowerCase();
     return /chrome/.test(userAgent) && /android/.test(userAgent);
 }
 
@@ -66,28 +66,55 @@ function setColorPalette() {
 }
 
 // ANIMATION V2
+
+function resetFirstTime() {
+    localStorage["visited"] = "false";
+}
 function animationSetup() {
+    let firstTime = false;
+    if(localStorage["visited"] !== "true") {
+        firstTime = true;
+        localStorage["visited"] = "true";
+    }
     let e1 = document.getElementById("svgText");
     let e2 = document.getElementById("svgDot");
     let l = document.getElementById("loadingAnimation");
     let t = document.getElementById("title");
-    setTimeout(function () {
-        e1.addEventListener("animationiteration", function () {
-            e1.classList.remove("start");
-            e2.classList.remove("start");
-            e1.classList.add("end");
-            e2.classList.add("end");
-            setTimeout(function () {
-                l.style.width = "clamp(200px, 90%, 1000px";
+    if (firstTime) {
+        console.log("first time");
+        setTimeout(function () {
+            e1.addEventListener("animationiteration", function () {
+                e1.classList.remove("start");
+                e2.classList.remove("start");
+                e1.classList.add("end");
+                e2.classList.add("end");
+                setTimeout(function () {
+                    l.style.width = "clamp(200px, 90%, 1000px)";
+                    document.getElementById("titleText").style.opacity = "1";
+                    document.querySelector(".imageContainer").style.opacity = "1";
+                }, 1500);
+                setTimeout(function () {
+                    t.classList.add("active");
+                    enableScroll(0);
+                }, 5500);
+            });
+        }, 4000);
+    }else{
+        console.log("not first time");
+        setTimeout(function () {
+            e1.addEventListener("animationiteration", function () {
+                e1.classList.remove("start");
+                e2.classList.remove("start");
+                e1.classList.add("end");
+                e2.classList.add("end");
+                l.style.width = "clamp(200px, 90%, 1000px)";
                 document.getElementById("titleText").style.opacity = "1";
                 document.querySelector(".imageContainer").style.opacity = "1";
-            }, 1500);
-            setTimeout(function () {
                 t.classList.add("active");
                 enableScroll(0);
-            }, 5500);
-        });
-    }, 4000);
+            });
+        }, 1000);
+    }
 }
 
 //document.getElementById("title").style.opacity = "1";
