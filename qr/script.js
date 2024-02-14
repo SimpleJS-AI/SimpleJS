7f3cabb7f50d35da3b6f169789526962e0f32034
flug8 <80972789+flug8@users.noreply.github.com>
2024-02-14 21:38:17 +0100

    added warning

--- qr/script.js
+++ qr/script.js
@@ -1,10 +1,30 @@
+let popoverSupported = true;
+
 function setup() {
     if (!HTMLElement.prototype.hasOwnProperty("popover")) {
         document.getElementById("popover").style.display = "none";
         console.log("popover")
+        popoverSupported = false;
     }
 }
 
+function run(id) {
+    id = window.innerWidth <= 1280 && id === 0 ? 3 : id;
+    switch (id) {
+        case 0:
+            window.open("https://www.google.com", "_self");
+            break;
+        case 1:
+            window.open("https://www.google.com", "_self");
+            break;
+        case 2:
+            window.open("https://www.google.com", "_self");
+            break;
+        case 3:
+            document.getElementById("popover").showPopover();
+    }
+
+}
 
 
 window.addEventListener("DOMContentLoaded", setup);
